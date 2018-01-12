cd23b1169315a17acc637e6d84bd5ffae97cc1f9
MichalNemec <nemec.michal@icloud.com>
2018-01-12 14:41:19 +0100

    assets fix

--- SortableGridViewAsset.php
+++ SortableGridViewAsset.php
@@ -5,7 +5,7 @@ namespace richardfan\sortable;
 use yii\web\AssetBundle;
 
 class SortableGridViewAsset extends AssetBundle {
-    public $sourcePath = '@vendor/richardfan1126/yii2-sortable-gridview/assets';
+    public $sourcePath = '@vendor/nexen/yii2-sortable-gridview/assets';
     public $baseUrl = '@web';
     public $js = [
         'js/jquery.ui.touch-punch.min.js',
